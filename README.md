c991b34a9268d88edd248a42b6d1ccd2b19c0356
Tj Woods <s402983@students.cdu.edu.au>
2026-09-15 17:35:18 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HIT137 - Software Now - Assessment 3 - Group DAN/EXT 09
+
+Kien Nguyen
+Tj Woods
+Andrew Yang
---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import opencv
+import random
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
